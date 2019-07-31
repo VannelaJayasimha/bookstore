d08bf769da6dd8acbfc1445018b1ed4ac4211d17
VannelaJayasimha <36077014+VannelaJayasimha@users.noreply.github.com>
2019-07-31 11:49:29 +0530

    Add files via upload

---
+++ week-1,2/valid.js
@@ -0,0 +1,22 @@
+
+function fun()
+{
+var a,b;
+a=document.getElementById("id1").value;
+b=document.getElementById("id2").value;
+p1=/[A-z]{3,}/;
+p1=/[A-z]{3,}/;
+r=p1.test(a);
+if(r==true)
+{
+//	if(p2.test(b)==true)
+	//{
+		alert("welecome");
+		return true;
+	
+	}
+	else{
+		alert("not valid");
+		return false;
+	}
+}
